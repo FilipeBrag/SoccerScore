3fc3d3845bbe0090a0d85be56822d2a9fc7a8774
Filipe Braga <filipebraga0505@gmail.com>
2026-05-05 19:29:04 -0300

    Feat: Adicionando a tela 3 junto com logica relevante

--- app/src/main/java/br/edu/ifsp/scl/sc3044807/soccerscore/ResultadoFinal.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044807/soccerscore/ResultadoFinal.kt
@@ -1,2 +1,24 @@
 package br.edu.ifsp.scl.sc3044807.soccerscore
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun resultScreen(timeA: String, timeB: String, golsA: Int, golsB: Int, onNovoJogo: () -> Unit){
+    val resultado = when {
+        golsA > golsB -> "$timeA venceu!"
+        golsB > golsA -> "$timeB venceu!"
+        else -> "Empate emocionante!"
+    }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(resultado, fontSize = 24.sp)
+        Button(onClick = onNovoJogo) { Text("Novo Jogo") }
+    }
+}
